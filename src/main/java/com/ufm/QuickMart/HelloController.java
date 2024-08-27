638ce4b1528cfe1c1f0ced94d450c76f064bb86f
DanyHidalgo <danielhl@ufm.edu>
2024-08-27 17:08:21 -0600

    hola mundo creado para comprobar conexiones en frontend

--- src/main/java/com/ufm/QuickMart/HelloController.java
+++ src/main/java/com/ufm/QuickMart/HelloController.java
@@ -1,15 +1,13 @@
 package com.ufm.QuickMart;
 
 import org.springframework.web.bind.annotation.GetMapping;
-import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
 @RestController
-@RequestMapping("/api")
 public class HelloController {
 
-    @GetMapping("/hello")
+    @GetMapping("/api/hola")
     public String sayHello() {
-        return "Hello, World!";
+        return "Hola Mundo";
     }
 }
